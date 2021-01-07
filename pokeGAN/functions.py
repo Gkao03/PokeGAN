7bd9e3e0442d76cbdb65b6a6995eeab2a4c631ff
Gore Kao <gkao@purdue.edu>
2021-01-07 00:34:40 -0800

    weights init function

---
+++ pokeGAN/functions.py
@@ -0,0 +1,15 @@
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn
+import os
+import random
+
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
