69114c273493afaebf38e26b7fa57f6507bfb8e3
Gore Kao <gkao@purdue.edu>
2021-01-08 05:01:20 -0800

    training initialization

--- pokeGAN/training.py
+++ pokeGAN/training.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
 import os
 from pokeGAN.functions import *
 from pokeGAN.models import *
@@ -25,4 +26,22 @@ def init_model(args):
 
 
 def train(args):
-    pass
+    device = get_device()
+
+    # create Generator and Discriminator
+    netG, netD = init_model(args)
+
+    # loss functions and optimizers. Change if needed
+    # DCGAN uses the binary cross entropy loss function
+    criterion = nn.BCELoss()
+
+    # create fixed noise for generator to see progression on single noise input
+    fixed_noise = torch.randn(args.ngf, args.nz, 1, 1, device=device)  # 64, 100, 1, 1 for DCGAN generator
+
+    # set real and fake labels
+    real_label = 1
+    fake_label = 0
+
+    # use ADAM optimizer. Change lr and beta1 based on DCGAN paper
+    optimizerG = optim.Adam(netG.parameters(), lr=args.lr, betas=(args.beta1, 0.999))
+    optimizerD = optim.Adam(netD.parameters(), lr=args.lr, betas=(args.beta1, 0.999))
