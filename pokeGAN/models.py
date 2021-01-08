2da51fb42f9d881fdbaa61f840cd0abac7345c38
Gore Kao <gkao@purdue.edu>
2021-01-07 22:43:43 -0800

    DCGAN discriminator init

--- pokeGAN/models.py
+++ pokeGAN/models.py
@@ -11,7 +11,7 @@ class DCGANGenerator(nn.Module):
     """
     def __init__(self, z_dim, features_g, channels_img):
         """
-        Constructor for DCGAN Generator
+        Constructor for DCGAN Generator.
         :param z_dim: size (dimension) of noise vector (100 for original DCGAN)
         :param features_g: size of feature maps in generator
         :param channels_img: number of channels in image (3 for RGB)
@@ -35,7 +35,8 @@ class DCGANGenerator(nn.Module):
 
     def _block(self, in_channels, out_channels, kernel_size, stride, padding):
         """
-        A sequential convolutional block from DCGAN
+        A sequential convolutional block from DCGAN for Generator.
+        Uses fractional-strided convolutions and ReLU activation.
         :param in_channels: num channels in input
         :param out_channels: num channels in output
         :param kernel_size: n x n size of kernel (4 x 4 for DCGAN)
@@ -52,6 +53,60 @@ class DCGANGenerator(nn.Module):
                                                     bias=False))
         block.add_module('batchnorm', nn.BatchNorm2d(out_channels))
         block.add_module('relu', nn.ReLU(inplace=True))
+        return block
+
+    def forward(self, x):
+        return self.main(x)
+
+
+class DCGANDiscriminator(nn.Module):
+    """
+    Discriminator architecture for DCGAN
+    """
+    def __init__(self, z_dim, features_g, channels_img):
+        """
+        Constructor for DCGAN Discriminator.
+        :param z_dim: size (dimension) of noise vector (100 for original DCGAN)
+        :param features_g: size of feature maps in generator
+        :param channels_img: number of channels in image (3 for RGB)
+        """
+        super(DCGANDiscriminator, self).__init__()
+        self.main = nn.Sequential()
+        channel_sizes = [features_g * 2 ** x for x in reversed(range(4))]  # [512, 256, 128, 64] -> 4 conv blocks
+
+        self.main.add_module('block1', self._block(z_dim, channel_sizes[0], 4, 1, 0))  # first conv block
+        for i, channel_size in enumerate(channel_sizes[1:]):  # other conv blocks
+            self.main.add_module('block%d' % (i + 2), self._block(channel_size * 2, channel_size, 4, 2, 1))
+
+        # last convolution
+        self.main.add_module('lastConv', nn.ConvTranspose2d(in_channels=features_g,
+                                                            out_channels=channels_img,
+                                                            kernel_size=4,
+                                                            stride=2,
+                                                            padding=1,
+                                                            bias=False))
+        self.main.add_module('tanh', nn.Tanh())  # [-1, 1]
+
+    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
+        """
+        A sequential convolutional block from DCGAN for Discriminator.
+        Uses strided convolution with leaky ReLU activation.
+        :param in_channels: num channels in input
+        :param out_channels: num channels in output
+        :param kernel_size: n x n size of kernel (4 x 4 for DCGAN)
+        :param stride: stride size
+        :param padding: padding size
+        :return: convolutional block including convolution, batchnorm, relu
+        """
+        block = nn.Sequential()
+        block.add_module('conv', nn.ConvTranspose2d(in_channels=in_channels,
+                                                    out_channels=out_channels,
+                                                    kernel_size=kernel_size,
+                                                    stride=stride,
+                                                    padding=padding,
+                                                    bias=False))
+        block.add_module('batchnorm', nn.BatchNorm2d(out_channels))
+        block.add_module('leakyrelu', nn.LeakyReLU(negative_slope=0.2, inplace=True))  # uses slope 0.2 in paper
         return block
 
     def forward(self, x):
