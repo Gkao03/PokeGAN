e1c4785388c92ce15714862e844c39239db01b9d
Gore Kao <gkao@purdue.edu>
2021-01-08 02:34:19 -0800

    training init models function

--- pokeGAN/training.py
+++ pokeGAN/training.py
@@ -3,7 +3,20 @@ import torch.nn as nn
 import torch.nn.functional as F
 import os
 from pokeGAN.functions import *
+from pokeGAN.models import *
 
 
-def init_model(arguments):
+def init_model(args):
     device = get_device()
+
+    netG = DCGANGenerator(args.nz, args.ngf, args.nc).to(device)
+    netG.apply(weights_init)
+    if args.netG != '':
+        netG.load_state_dict(torch.load(args.netG))
+    print(netG)
+
+    netD = DCGANDiscriminator(args.ndf, args.nc)
+    netD.apply(weights_init)
+    if args.netD != '':
+        netD.load_state_dict(torch.load(args.netD))
+    print(netD)
