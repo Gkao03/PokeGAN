remove incorrect image normalization

main_train.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     plt.subplot(1, 2, 2)
     plt.axis("off")
     plt.title("Fake Images")
-    fake_image_grid = img_list[-1] * 0.5 + 0.5
+    fake_image_grid = img_list[-1]
     plt.imshow(np.transpose(fake_image_grid, (1, 2, 0)))
     # plt.show()
 

pokeGAN/training.py
@@ -43,7 +43,7 @@ def train(args):
     criterion = nn.BCELoss()
 
     # create fixed noise for generator to see progression on single noise input
-    fixed_noise = torch.randn(args.ngf, args.nz, 1, 1, device=device)  # 64, 100, 1, 1 for DCGAN generator
+    fixed_noise = torch.randn(args.ngf, args.nz, 1, 1, device=device)
 
     # real and fake labels
     # ORIGINAL